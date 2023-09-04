217d818e0677a8c2e619861de58faeab26b465e7
Lucas Jose <lucas@ati.to.gov.br>
2023-09-04 14:46:46 -0300

    Primeiro commit

---
+++ src/main/java/com/lucas/rastreio/domain/model/Package.java
@@ -0,0 +1,79 @@
+package com.lucas.rastreio.domain.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="packages")
+public class Package {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String codigo;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    private LocalDate initDate;
+    
+    private LocalDate finalDate;
+
+
+
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getInitDate() {
+        return initDate;
+    }
+
+    public void setInitDate(LocalDate initDate) {
+        this.initDate = initDate;
+    }
+
+    public LocalDate getFinalDate() {
+        return finalDate;
+    }
+
+    public void setFinalDate(LocalDate finalDate) {
+        this.finalDate = finalDate;
+    }
+
+
+    
+
+}
